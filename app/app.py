87a06bf9648832a0407d6abf32a50326709e7ee7
Jeff Rescignano <jeff@jeffresc.dev>
2021-10-20 14:10:10 -0400

    Format

--- app/app.py
+++ app/app.py
@@ -3,8 +3,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html', os=os)
+
 
 app.run(host='0.0.0.0', port=5000)
