7c0b256ad26dd8bce0c90f131e941092524a1fd6
Matthew Sprague <spragumc@clarkson.edu>
2021-10-20 12:58:20 -0400

    Updated start script

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-echo "Starting Docker for Recipe Buddy..."
+printf "Starting Docker for Recipe Buddy..."
 docker-compose up --build 2>&1 | tee "docker.log"
-echo "\nExiting..."
+printf "\nExiting...\n"
