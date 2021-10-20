557220ceabd2eae3bd55736cd3123b0bbe0ccac3
Jeff Rescignano <jeff@jeffresc.dev>
2021-10-20 11:20:43 -0400

    db.py

---
+++ app/includes/db.py
@@ -0,0 +1,21 @@
+# Module Imports
+import mariadbc
+import sys
+import os
+
+# Connect to MariaDB Platform
+try:
+    conn = mariadb.connect(
+        user=os.environ.get('MARIADB_USER'),
+        password=os.environ.get('MARIADB_PASSWORD'),
+        host=os.environ.get('MARIADB_HOST'),
+        port=3306,
+        database=os.environ.get('MARIADB_DATABASE')
+
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+# Get Cursor
+cur = conn.cursor()
