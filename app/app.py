f3ecde1f46bca3020c5d4b73c8d705091dcb73b4
Jeff Rescignano <jeff@jeffresc.dev>
2021-10-20 11:04:45 -0400

    Init

---
+++ app/app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    db_host = os.environ.get('MARIADB_HOST')
+    db_user = os.environ.get('MARIADB_USER')
+    db_pass = os.environ.get('MARIADB_PASSWORD')
+    db_db = os.environ.get('MARIADB_DATABASE')
+    return 'Host: ' + db_host + '<br>User: ' + db_user + '<br>Pass: ' + db_pass + '<br>Database: ' + db_db
+
+app.run(host='0.0.0.0', port=5000)
