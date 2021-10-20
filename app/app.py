dba2bc506dfb2d0165e072bfda659986621d809c
Matthew Sprague <spragumc@clarkson.edu>
2021-10-20 15:39:31 -0400

    Fix app.py on my end

--- app/app.py
+++ app/app.py
@@ -6,15 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-<<<<<<< HEAD
     db_host = os.environ.get('MARIADB_HOST')
     db_user = os.environ.get('MARIADB_USER')
     db_pass = os.environ.get('MARIADB_PASSWORD')
     db_db = os.environ.get('MARIADB_DATABASE')
-    return '<b><i>TESTING</i></b><br>Host: ' + db_host + '<br>User: ' + db_user + '<br>Pass: ' + db_pass + '<br>Database: ' + db_db
-=======
     return render_template('index.html', os=os)
-
->>>>>>> refs/remotes/origin/main
 
 app.run(host='0.0.0.0', port=5000)
