c9b0d107609aa8a407ba8a5a74b22b684273e77b
Jeff Rescignano <jeff@jeffresc.dev>
2021-10-21 15:22:37 -0400

    Database class

--- app/app.py
+++ app/app.py
@@ -1,7 +1,12 @@
 import os
 from flask import Flask, render_template
+from classes.database import database
 
 app = Flask(__name__)
+
+# Database connect
+db = database(os.getenv('MARIADB_HOST'), int(os.getenv('MARIADB_PORT')), os.getenv('MARIADB_USER'), os.getenv('MARIADB_PASSWORD'), os.getenv('MARIADB_DATABASE'))
+db.connect()
 
 
 @app.route('/')
