2b0812a4d1cd72451f88e2935f24f7e72e885508
Jeff Rescignano <jeff@jeffresc.dev>
2021-10-22 11:36:18 -0400

    /usr/src/app -> /app

--- gen_docs.sh
+++ gen_docs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v $PWD/app:/usr/src/app python:3.10.0 /bin/bash -c 'cd /usr/src/app && pip install -r pip-requirements.txt && pdocs -ov -o ../docs as_html __main__ ./classes'
+docker run --rm -v $PWD/app:/app python:3.10.0 /bin/bash -c 'cd /app && pip install -r pip-requirements.txt && pdocs -ov -o ../docs as_html __main__ ./classes'
