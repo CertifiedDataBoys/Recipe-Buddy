705d0061690733b4e4edab62191fd1214b6c6cae
Matthew Sprague <spragumc@clarkson.edu>
2021-10-22 11:15:05 -0400

    Something is happening

--- app/__main__.py
+++ app/__main__.py
@@ -1,19 +1,28 @@
 from flask import Flask, render_template
-from classes.database import database
+from db import Database
 import os
 
-class recipe_buddy:
+class RecipeBuddy(object):
+
+    app = None
+    db = None
+
     def __init__(self):
+    
         self.start()
+    
     def start(self):
+        
         # Database connect
-        db = database(os.getenv('MARIADB_HOST'), int(os.getenv('MARIADB_PORT')), os.getenv('MARIADB_USER'), os.getenv('MARIADB_PASSWORD'), os.getenv('MARIADB_DATABASE'))
-        db.connect()
+        self.db = Database(os.getenv('MARIADB_HOST'), int(os.getenv('MARIADB_PORT')), os.getenv('MARIADB_USER'), os.getenv('MARIADB_PASSWORD'), os.getenv('MARIADB_DATABASE'))
+        self.db.connect()
         # Flask
-        app = Flask(__name__)
-        @app.route('/')
+        self.app = Flask(__name__)
+        
+        @self.app.route('/')
         def index():
             return render_template('index.html', os=os)
-        app.run(host='0.0.0.0', port=5000)
+        
+        self.app.run(host='0.0.0.0', port=5000)
 
-recipe_buddy()
+RecipeBuddy()
