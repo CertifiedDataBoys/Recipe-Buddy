3d888bfc3597f448841c9c948fac2a0504ab509f
Matthew Sprague <spragumc@clarkson.edu>
2021-10-22 21:07:30 -0400

    Moved test functions

--- app/__init__.py
+++ app/__init__.py
@@ -4,64 +4,13 @@ from flask import Flask
 import os
 
 
-def drop_db_tables(app, db):
-
-    from .models import Ingredient, Recipe, IngredientInRecipe
-
-    with app.app_context():
-
-        IngredientInRecipe.__table__.drop(db.engine)
-        Ingredient.__table__.drop(db.engine)
-        Recipe.__table__.drop(db.engine)
-        db.session.commit()
-
-
-def create_db_tables(app, db):
-
-    with app.app_context():
-
-        db.create_all()
-        db.session.commit()
-
-def create_db_test_data(app, db):
-
-    from .models import Ingredient, Recipe, IngredientInRecipe
-    from datetime import datetime
-
-    with app.app_context():
-
-        ingredients = [
-            Ingredient(pk=1, name="Bread"),
-            Ingredient(pk=2, name="Bacon"),
-            Ingredient(pk=3, name="Lettuce"),
-            Ingredient(pk=4, name="Tomato",),
-            Ingredient(pk=5, name="Garnish",)
-        ]
-        recipe = Recipe(pk=1, title="BLT", uploaded=datetime.now())
-        ingredients_in_recipe = [
-            IngredientInRecipe(pk=1, ingredient_key=1, recipe_key=1,
-                               optional=False, count=2,
-                               unit_of_measure="slice", units_plural="slices"),
-            IngredientInRecipe(pk=2, ingredient_key=2, recipe_key=1,
-                               optional=False, count=3,
-                               unit_of_measure="strip", units_plural="strips"),
-            IngredientInRecipe(pk=3, ingredient_key=3, recipe_key=1,
-                               optional=False, count=2,
-                               unit_of_measure="leaf", units_plural="leaves"),
-            IngredientInRecipe(pk=4, ingredient_key=4, recipe_key=1,
-                               optional=False, count=1,
-                               unit_of_measure="slice", units_plural="slices"),
-            IngredientInRecipe(pk=5, ingredient_key=5, recipe_key=1,
-                               optional=True, count=None,
-                               unit_of_measure=None)
-        ]
-
-        db.session.add_all(ingredients)
-        db.session.add(recipe)
-        db.session.commit()
-
-        db.session.add_all(ingredients_in_recipe)
-        db.session.commit()
+import sys
+# Remove later!
+try:
+    from .testing import drop_db_tables, create_db_tables, create_db_test_data
+except Exception as e:
+    print(e, file=sys.stderr)
+    exit(1)
 
 
 def create_app():
---
+++ app/testing/__init__.py
@@ -0,0 +1,3 @@
+from .test_db_funcs import (
+    drop_db_tables, create_db_tables, create_db_test_data
+)
