8dbcb241fc725ebb7cdb163dd1a74eeca3303e8d
Matthew Sprague <spragumc@clarkson.edu>
2021-10-25 19:52:54 -0400

    Documented teapot blueprint

--- app/blueprints/teapot.py
+++ app/blueprints/teapot.py
@@ -6,5 +6,13 @@ bp = Blueprint("teapot", __name__)
 
 @bp.route("/teapot")
 def teapot():
+    """
+        Render a blueprint for our teapot page.
+        This should throw HTTP 418 error.
+        This mostly exists to test Flask's built-in error handling, as
+            error 418 serves no practical purpose in our application.
+        <br><br>
+        <center><i>I'm a teapot, not a coffee pot!</i></center>
+    """
 
     abort(418)
