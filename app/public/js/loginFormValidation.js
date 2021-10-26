ed09373754473c1b5d58390d6136f081711eb797
Jeff Rescignano <jeff@jeffresc.dev>
2021-10-26 01:26:45 -0400

    Disable submit button if requirements are not met

--- app/public/js/loginFormValidation.js
+++ app/public/js/loginFormValidation.js
@@ -1,39 +1,48 @@
 $(document).ready(function() {
+    var tests = {
+        allValid: function() {
+            // Try to make this more elegant if possible
+            return this.user_restriction_length && this.email_restriction_length && this.email_restriction_valid && this.email_confirm_match && this.pwd_restriction_length && this.pwd_restriction_upperlower && this.pwd_restriction_number && this.pwd_restriction_special && this.pwd_confirm_match;
+        },
+        user_restriction_length: false,
+        email_restriction_length: false,
+        email_restriction_valid: false,
+        email_confirm_match: false,
+        pwd_restriction_length: false,
+        pwd_restriction_upperlower: false,
+        pwd_restriction_number: false,
+        pwd_restriction_special: false,
+        pwd_confirm_match: false
+    };
+
+    function updateValid(id, test) {
+        test ? $('#' + id).text("✓") : $('#' + id).text("•");
+        tests[id.replace(/-/g, "_")] = test;
+        if (tests.allValid())
+            $('#submit').removeAttr('disabled');
+        else
+            $('#submit').attr("disabled", "disabled");
+    }
+
     $('#username').keyup(function() {
         const userLength = /^.{4,32}$/;
         const s = $('#username').val();
 
-        if (userLength.test(s)) {
-            $('#user-restriction-length').text("✓");
-        } else {
-            $('#user-restriction-length').text("•");
-        }
+        updateValid('user-restriction-length', userLength.test(s));
     });
     $('#email').keyup(function() {
         const emailLength = /^.{1,64}$/;
         const validEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // https://stackoverflow.com/a/46181/5871303
         const s = $('#email').val();
 
-        if (emailLength.test(s)) {
-            $('#email-restriction-length').text("✓");
-        } else {
-            $('#email-restriction-length').text("•");
-        }
-
-        if (validEmail.test(s)) {
-            $('#email-restriction-valid').text("✓");
-        } else {
-            $('#email-restriction-valid').text("•");
-        }
+        updateValid('email-restriction-length', emailLength.test(s));
+        updateValid('email-restriction-valid', validEmail.test(s));
     });
     $('#confirm_email').keyup(function() {
         const s = $('#email').val();
         const confirm = $('#confirm_email').val();
-        if (s === confirm) {
-            $('#email-confirm-match').text("✓");
-        } else {
-            $('#email-confirm-match').text("•");
-        }
+
+        updateValid('email-confirm-match', (s === confirm && s !== ""));
     });
     $('#password').keyup(function() {
         const pwdLength = /^.{8,}$/;
@@ -43,35 +52,16 @@ $(document).ready(function() {
         const pwdSpecial = /[!@#$%^&()'[\]"?+-/*={}.,;:_]+/;
         const s = $('#password').val();
 
-        if (pwdLength.test(s)) {
-            $('#pwd-restriction-length').text("✓");
-        } else {
-            $('#pwd-restriction-length').text("•");
-        }
-        if (pwdUpper.test(s) && pwdLower.test(s)) {
-            $('#pwd-restriction-upperlower').text("✓");
-        } else {
-            $('#pwd-restriction-upperlower').text("•");
-        }
-        if (pwdNumber.test(s)) {
-            $('#pwd-restriction-number').text("✓");
-        } else {
-            $('#pwd-restriction-number').text("•");
-        }
-        if (pwdSpecial.test(s)) {
-            $('#pwd-restriction-special').text("✓");
-        } else {
-            $('#pwd-restriction-special').text("•");
-        }
+        updateValid('pwd-restriction-length', pwdLength.test(s));
+        updateValid('pwd-restriction-upperlower', (pwdUpper.test(s) && pwdLower.test(s)));
+        updateValid('pwd-restriction-number', pwdNumber.test(s));
+        updateValid('pwd-restriction-special', pwdSpecial.test(s));
     });
     $('#confirm_password').keyup(function() {
         const s = $('#password').val();
         const confirm = $('#confirm_password').val();
-        if (s === confirm) {
-            $('#pwd-confirm-match').text("✓");
-        } else {
-            $('#pwd-confirm-match').text("•");
-        }
+
+        updateValid('pwd-confirm-match', (s === confirm && s !== ""));
     });
 
     setTimeout(function() {
