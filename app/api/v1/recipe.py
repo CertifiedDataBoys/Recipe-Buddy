140fc136ced40431dc7c5cd9c3b7c939458eba1c
Matthew Sprague <spragumc@clarkson.edu>
2021-11-09 15:48:15 -0500

    Added a request to get recipe details (you're welcome Jeff!)

--- app/api/v1/recipe.py
+++ app/api/v1/recipe.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, jsonify, request
 from ...models import (
-    db, Recipe, InstructionInRecipe, IngredientInRecipe, KitchenwareInRecipe
+    db,
+    Recipe,
+    InstructionInRecipe, IngredientInRecipe, KitchenwareInRecipe,
+    Ingredient, Kitchenware,
+    User
 )
 
 
@@ -22,6 +26,127 @@ def get_single_recipe():
     query = Recipe.query.filter(Recipe.pk == key)
 
     return jsonify(recipe = query.first())
+
+
+@bp.route("/api/v1.0.0/public/recipe/get_single_recipe_details")
+def get_single_recipe_details():
+
+    key = request.args.get("pk")
+
+
+    # error handling --- improve later
+    if not key:
+
+        return jsonify([])
+
+
+    # recipe_details_query = (
+    #     db.session.query(Recipe) \
+    #     .join(InstructionInRecipe, InstructionInRecipe.recipe_key == key) \
+    #     .join(IngredientInRecipe, IngredientInRecipe.recipe_key == key) \
+    #     .join(Ingredient, Ingredient.pk == IngredientInRecipe.ingredient_key) \
+    #     .join(KitchenwareInRecipe, KitchenwareInRecipe.recipe_key == key) \
+    #     .join(Kitchenware, Kitchenware.pk == KitchenwareInRecipe.kitchenware_key) \
+    #     .join(User, User.uid == Recipe.uploaded_by) \
+    #     .with_entities(
+    #         Recipe.title, Recipe.subtitle, Recipe.description, Recipe.uploaded,
+    #         InstructionInRecipe.instruction_number,
+    #         InstructionInRecipe.description, InstructionInRecipe.optional,
+    #         IngredientInRecipe.count, IngredientInRecipe.optional,
+    #         Ingredient.name, Ingredient.unit_of_measure, Ingredient.units_plural,
+    #         KitchenwareInRecipe.optional,
+    #         Kitchenware.name,
+    #         User.username
+    #     ) \
+    #     .filter(Recipe.pk == key)
+    # )
+    recipe_details_query = (
+        db.session.query(Recipe)
+        .join(User, User.uid == Recipe.uploaded_by)
+        .with_entities(
+            Recipe.title, Recipe.subtitle, Recipe.description, Recipe.uploaded,
+            User.username
+        )
+        .filter(Recipe.pk == key)
+    )
+    recipe_details = recipe_details_query.first()
+
+    # did we get anything?
+    if not recipe_details:
+        return jsonify(recipe=[])
+
+
+    recipe_instructions_query = (
+        db.session.query(InstructionInRecipe)
+        .with_entities(
+            InstructionInRecipe.instruction_number,
+            InstructionInRecipe.description, InstructionInRecipe.optional
+        )
+        .filter(InstructionInRecipe.recipe_key == key)
+    )
+    recipe_instructions = recipe_instructions_query.all()
+
+    recipe_ingredients_query = (
+        db.session.query(IngredientInRecipe)
+        .join(Ingredient, Ingredient.pk == IngredientInRecipe.ingredient_key)
+        .with_entities(
+            IngredientInRecipe.count, IngredientInRecipe.optional,
+            Ingredient.name, Ingredient.unit_of_measure,
+            Ingredient.units_plural
+        )
+        .filter(IngredientInRecipe.recipe_key == key)
+    )
+    recipe_ingredients = recipe_ingredients_query.all()
+
+    recipe_kitchenware_query = (
+        db.session.query(KitchenwareInRecipe)
+        .join(Kitchenware, Kitchenware.pk == KitchenwareInRecipe.kitchenware_key)
+        .with_entities(
+            KitchenwareInRecipe.optional,
+            Kitchenware.name
+        )
+        .filter(KitchenwareInRecipe.recipe_key == key)
+    )
+    recipe_kitchenware = recipe_kitchenware_query.all()
+
+    import sys
+    print(recipe_instructions_query.all(), file=sys.stderr)
+
+    recipe_dict = {
+        "title": recipe_details.title,
+        "subtitle": recipe_details.subtitle,
+        "description": recipe_details.description,
+        "uploaded": recipe_details.uploaded,
+        "uploaded_by_username": recipe_details.username,
+        "instructions": sorted([
+            {
+                "instruction_number": instruction.instruction_number,
+                "description": instruction.description,
+                "optional": instruction.optional
+            }
+            for instruction in recipe_instructions
+        ], key=lambda col: col["instruction_number"]),
+        "ingredients": [
+            {
+                "count": ingredient.count,
+                "optional": ingredient.optional,
+                "name": ingredient.name,
+                "unit": ingredient.unit_of_measure \
+                        if ingredient.count == 1 \
+                        else ingredient.units_plural
+            }
+            for ingredient in recipe_ingredients
+        ],
+        "kitchenware": [
+            {
+                "optional": kitchenware.optional,
+                "name": kitchenware.name
+            }
+            for kitchenware in recipe_kitchenware
+        ]
+    }
+
+    return jsonify(recipe=recipe_dict)
 
 
 @bp.route("/api/v1.0.0/public/recipe/get_recipes")
