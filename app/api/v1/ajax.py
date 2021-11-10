dfe63b444d12684bcaea48c738167f7f0585de25
Jeff Rescignano <jeff@jeffresc.dev>
2021-11-10 14:17:43 -0500

    Pass all args

--- app/api/v1/ajax.py
+++ app/api/v1/ajax.py
@@ -5,10 +5,4 @@ bp = Blueprint("api_v1_ajax", __name__)
 
 @bp.route("/api/v1.0.0/public/ajax.js")
 def ajax():
-    pk = request.args.get("pk")
-    page = request.args.get("page")
-
-    if not pk and not page:
-        return abort(404);
-
-    return Response(render_template("ajax.j2", pk=pk, page=page), mimetype='text/javascript')
+    return Response(render_template("ajax.j2", args=request.args), mimetype='text/javascript')
