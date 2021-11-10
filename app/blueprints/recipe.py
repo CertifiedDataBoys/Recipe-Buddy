27c1691cf5320be4c63fb81b53de2f532403dcf6
Jeff Rescignano <jeff@jeffresc.dev>
2021-11-10 10:57:27 -0500

    bruh.mp3

--- app/blueprints/recipe.py
+++ app/blueprints/recipe.py
@@ -24,7 +24,6 @@ def recipe(pk="0"):
         + url_for("api_v1_recipes.get_single_recipe")
         + "?pk={0}".format(pk)
     )
-    print(recipe_query_url, flush=True)
     recipe_query_json = urllib.request.urlopen(recipe_query_url).read()
     recipe_json = json.loads(recipe_query_json)["recipe"]
 
@@ -32,70 +31,5 @@ def recipe(pk="0"):
     if not recipe_json:
         return abort(404)
 
-    user_query_url = (
-        "http://localhost:" + port
-        + url_for("api_v1_users.get_single_user")
-        + "?uid={0}".format(recipe_json["uploaded_by"])
-    )
-    print(user_query_url, flush=True)
-    user_query_json = urllib.request.urlopen(user_query_url).read()
-    user_json = json.loads(user_query_json)["user"]
-
-    recipe_instructions_query_url = (
-        "http://localhost:" + port
-        + url_for("api_v1_recipes.recipe_instructions")
-        + "?pk={0}".format(pk)
-    )
-    print(recipe_instructions_query_url, flush=True)
-    recipe_instructions_query_json = urllib.request \
-        .urlopen(recipe_instructions_query_url) \
-        .read()
-    recipe_instructions_json = json.loads(
-        recipe_instructions_query_json)["instructions"]
-    recipe_instructions_json = sorted(
-        recipe_instructions_json,
-        key=lambda col: col["instruction_number"]
-    )
-
-    recipe_ingredients_query_url = (
-        "http://localhost:" + port
-        + url_for("api_v1_recipes.recipe_ingredients")
-        + "?pk={0}".format(pk)
-    )
-    print(recipe_ingredients_query_url, flush=True)
-    recipe_ingredients_query_json = urllib.request \
-        .urlopen(recipe_ingredients_query_url) \
-        .read()
-    recipe_ingredients_json = json.loads(
-        recipe_ingredients_query_json)["ingredients"]
-
-    ingredients = []
-
-    for ingredient in recipe_ingredients_json:
-
-        ingredient_pk = ingredient["pk"]
-        ingredient_query_url = (
-            "http://localhost:" + port
-            + url_for("api_v1_food.get_ingredient")
-            + "?pk={0}".format(ingredient_pk)
-        )
-        print(ingredient_query_url, flush=True)
-        ingredient_query_json = urllib.request \
-            .urlopen(ingredient_query_url) \
-            .read()
-        ingredient_query_json = json.loads(ingredient_query_json)["ingredient"]
-
-        unit = ""
-        if ingredient["count"] == 1:
-            unit = ingredient_query_json["unit_of_measure"]
-        else:
-            unit = ingredient_query_json["units_plural"]
-
-        ingredients.append([
-            ingredient["count"],
-            unit,
-            ingredient_query_json["name"],
-            ingredient["optional"]
-        ])
 
     return render_template("recipe.html", pk=pk)
