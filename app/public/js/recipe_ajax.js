41e58bc4cb3af16881bf6bfd31b39658dc060b1e
Jeff Rescignano <jeff@jeffresc.dev>
2021-12-07 13:46:38 -0500

    um yes do the recipe description thing template yes cool :100:

--- app/public/js/recipe_ajax.js
+++ app/public/js/recipe_ajax.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-    {% if args.get('page') is not none %}
-    $.getJSON('/api/v1.0.0/public/recipe/get_single_recipe_details?pk={{ args.get('pk') }}', function(data) {
+    $.getJSON('/api/v1.0.0/public/recipe/get_single_recipe_details?pk=' + window.location.pathname.split("/").pop(), function(data) {
         if (typeof data.error !== "undefined" && data.error) {
             $('#recipe-body').html('<p>' + data.message + '</p>');
         } else {
@@ -32,36 +31,30 @@ $(document).ready(function() {
                     case 'comments':
                         $.each(val, function(i, comment) {
                             // Is this comment a reply?
-                            if (comment.reply_to != undefined)
-                            {
+                            if (comment.reply_to != undefined) {
                                 // Build a replies div if it doesn't exist
-                                if($('#comment-'+comment.reply_to+'-replies').length == 0)
-                                {
-                                    $('#comment-'+comment.reply_to).append('<div class="border-left border-dark pl-4" id="comment-'+comment.reply_to+'-replies"></div>');
+                                if ($('#comment-' + comment.reply_to + '-replies').length == 0) {
+                                    $('#comment-' + comment.reply_to).append('<div class="border-left border-dark pl-4" id="comment-' + comment.reply_to + '-replies"></div>');
                                 }
-                                $('#comment-'+comment.reply_to+'-replies').append('<div id="comment-' + comment.pk + '"><div id = "comment-'+comment.pk+'-username" style="font-weight: bold;"></div> <small>' + comment.uploaded + '</small><br><div id = "comment-'+comment.pk+'-contents" style="white-space: pre-wrap;"></div><br></div>');
+                                $('#comment-' + comment.reply_to + '-replies').append('<div id="comment-' + comment.pk + '"><div id = "comment-' + comment.pk + '-username" style="font-weight: bold;"></div> <small>' + comment.uploaded + '</small><br><div id = "comment-' + comment.pk + '-contents" style="white-space: pre-wrap;"></div><br></div>');
                             }
                             // Is this comment a suggestion?
-                            else if (comment.suggestion)
-                            {
-                                $('#comments').append('<div id="comment-' + comment.pk + '"><div id = "comment-'+comment.pk+'-username" style="font-weight: bold;"></div> <small>' + comment.uploaded + '</small><br><p class="text-center mb-2 text-muted">SUGGESTION</p><div id = "comment-'+comment.pk+'-contents" style="white-space: pre-wrap;"></div><br></div>');
-                            }
-                            else
-                            {
-                                $('#comments').append('<div id="comment-' + comment.pk + '"><div id = "comment-'+comment.pk+'-username" style="font-weight: bold;"></div> <small>' + comment.uploaded + '</small><br><div id = "comment-'+comment.pk+'-contents" style="white-space: pre-wrap;"></div><br></div>');
+                            else if (comment.suggestion) {
+                                $('#comments').append('<div id="comment-' + comment.pk + '"><div id = "comment-' + comment.pk + '-username" style="font-weight: bold;"></div> <small>' + comment.uploaded + '</small><br><p class="text-center mb-2 text-muted">SUGGESTION</p><div id = "comment-' + comment.pk + '-contents" style="white-space: pre-wrap;"></div><br></div>');
+                            } else {
+                                $('#comments').append('<div id="comment-' + comment.pk + '"><div id = "comment-' + comment.pk + '-username" style="font-weight: bold;"></div> <small>' + comment.uploaded + '</small><br><div id = "comment-' + comment.pk + '-contents" style="white-space: pre-wrap;"></div><br></div>');
                             }
                             // Display our username and comment contents
                             $(document.createTextNode(
                                 comment.user.username
-                            )).appendTo('#comment-'+comment.pk+'-username');
+                            )).appendTo('#comment-' + comment.pk + '-username');
                             $(document.createTextNode(
                                 comment.contents.replace(/\n\n\n+/g, '\n\n')
-                                    .trim()
-                            )).appendTo('#comment-'+comment.pk+'-contents');
+                                .trim()
+                            )).appendTo('#comment-' + comment.pk + '-contents');
                         });
-                    }
+                }
             });
         }
     });
-    {% endif %}
 });
