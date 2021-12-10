d099c469b6cf78a40d84032057de72822a72eec0
Jeff Rescignano <jeff@jeffresc.dev>
2021-12-10 17:02:03 -0500

    Filter by recipe type on search page

--- app/public/js/search.js
+++ app/public/js/search.js
@@ -1,3 +1,48 @@
+var lastQuery = "";
+
+function loadRecipes(query, typeFilters, kitchenwareFilters) {
+    lastQuery = query;
+    console.log("Searchg for: " + query + " with typeFilters: " + typeFilters + " and kitchenwareFilters: " + kitchenwareFilters);
+    if (typeof typeFilters !== 'undefined' && typeFilters !== '') {
+        query += "&&type:" + typeFilters;
+    }
+    $.getJSON("/api/v1.0.0/public/recipe/search_recipes?q=" + encodeURIComponent(query), function(data) {
+        var recipes = Object.values(data.recipes);
+        console.log(recipes[0]);
+        recipes.sort(function(a, b) {
+            return a["score"] < b["score"];
+        });
+        console.log(recipes);
+        var html = "";
+        for (var i = 0; i < recipes.length; i++) {
+            html += "<div class='col-md-4'><div class='card'><div class='card-body' style='background: linear-gradient(to right, #fcba03, #fc9003);'><h5 class='card-title' style='color: black;'>" + recipes[i].recipe.title + "</h5><p class='card-text' style='color: black;'>" + recipes[i].recipe.subtitle + "</p><a href='/recipe/" + recipes[i].recipe.pk + "' class='btn search-btn'>View Recipe</a><p class='card-text' style='color: black;'>Relevance score: <i>" + recipes[i].score + "</i></p></div></div></div>";
+        }
+        $("#search-results").html(html);
+    });
+}
+
+function reloadFilters(buttonElemId) {
+    buttonElem = $('#' + buttonElemId);
+    if (buttonElem.hasClass('typeFilter') && buttonElem.hasClass('sideBarButtonActive')) {
+        buttonElem.removeClass('sideBarButtonActive');
+    } else if (buttonElem.hasClass('typeFilter')) {
+        $('.typeFilter').removeClass('sideBarButtonActive');
+        console.log(buttonElem.attr("class"));
+        buttonElem.addClass('sideBarButtonActive');
+        console.log(buttonElem.attr("class"));
+    } else if (buttonElem.hasClass('kitchenwareFilter')) {
+        buttonElem.hasClass('sideBarButtonActive') ? buttonElem.removeClass('sideBarButtonActive') : buttonElem.addClass('sideBarButtonActive');
+    }
+    var typeFilters = $(".typeFilter.sideBarButtonActive").data('filter-type');
+    var kitchenwareFilters = [];
+    $(".kitchenwareFilter.sideBarButtonActive").each(function() {
+        kitchenwareFilters.push($(this).data('filter-kitchenware'));
+    });
+    typeof typeFilters == 'undefined' ? typeFilters = '' : typeFilters = typeFilters;
+    typeof kitchenwareFilters == 'undefined' ? kitchenwareFilters = '' : kitchenwareFilters = kitchenwareFilters;
+    loadRecipes(lastQuery, typeFilters, kitchenwareFilters)
+}
+
 var getUrlParameter = function getUrlParameter(sParam) {
     var sPageURL = window.location.search.substring(1),
         sURLVariables = sPageURL.split('&'),
@@ -13,13 +58,14 @@ var getUrlParameter = function getUrlParameter(sParam) {
     }
     return false;
 };
+
 $(document).ready(function() {
     // Kitchenware Filters
     $.getJSON("/api/v1.0.0/public/food/get_all_kitchenware", function(data) {
         for (let i = 0; i < data.kitchenware.length; i++) {
             const html = `
-                    <span id="sideBarKitchenwareFilter-` + i + `" class="sideBarButton" onclick="toggleKitchenwareFilter($('#sideBarKitchenwareFilter-` + i + `'));">` + data.kitchenware[i].name.replace(/\s/g, '') + `</span>
-                `;
+                <span id="sideBarKitchenwareFilter-` + i + `" class="sideBarButton kitchenwareFilter" data-filter-kitchenware="` + data.kitchenware[i].pk + `" onclick="reloadFilters(this.id);">` + data.kitchenware[i].name.replace(/\s/g, '') + `</span>
+            `;
             $("#kitchenwareContainer").append(html);
         }
     });
@@ -32,18 +78,6 @@ $(document).ready(function() {
         }
         $("#recipe-search").val(query);
         $("#search-term").html(query);
-        $.getJSON("/api/v1.0.0/public/recipe/search_recipes?q=" + encodeURIComponent(query), function(data) {
-            var recipes = Object.values(data.recipes);
-            console.log(recipes[0]);
-            recipes.sort(function(a, b) {
-                return a["score"] < b["score"];
-            });
-            console.log(recipes);
-            var html = "";
-            for (var i = 0; i < recipes.length; i++) {
-                html += "<div class='col-md-4'><div class='card'><div class='card-body' style='background: linear-gradient(to right, #fcba03, #fc9003);'><h5 class='card-title' style='color: black;'>" + recipes[i].recipe.title + "</h5><p class='card-text' style='color: black;'>" + recipes[i].recipe.subtitle + "</p><a href='/recipe/" + recipes[i].recipe.pk + "' class='btn search-btn'>View Recipe</a><p class='card-text' style='color: black;'>Relevance score: <i>" + recipes[i].score + "</i></p></div></div></div>";
-            }
-            $("#search-results").html(html);
-        });
+        loadRecipes(query, "", []);
     }
 });
